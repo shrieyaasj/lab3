40d85d8761f072f12d86fb0887566097c80ed3ab
shrieyaasj <shrieyaa1512@gmail.com>
2023-10-24 17:41:47 -0700

    Updated ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,20 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test 
+  public void testReversed2() {
+    int[] input1 = {3,4,5,6};
+    assertArrayEquals(new int[] {6,5,4,3}, ArrayExamples.reversed(input1));
+  
+  }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input1 = {3,4,5,6};
+    ArrayExamples.reverseInPlace(input1);
+    System.out.println(input1);
+    assertArrayEquals(new int[]{6,5,4,3}, input1);
+	}
+
 }
